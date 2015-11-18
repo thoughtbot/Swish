814f4e5c3e3c5d78656e8c80cb6ffeffd7bbc51a
Gordon Fontenot <gmoney@gitpushgitpaid.com>
2015-11-18 14:55:09 -0800

    Add Swish specific error creation
    We were previously using Result's error creation method. This wasn't
    ideal, since the domain for errors _we_ were displaying ended up being
    `com.antitypical.result`. Instead, we should be creating our own errors
    with the proper domain info.
    
    This also helps remove some type info from where these errors are
    created.
    
    This commit also includes some general Xcode bullshittery since
    apparently adding a single file to the index is a massive task that
    requires rewriting most of the project file.

--- Source/Models/APIClient.swift
+++ Source/Models/APIClient.swift
@@ -24,13 +24,13 @@ private func deserialize(response: HTTPResponse) -> Result<JSON, NSError> {
   case 200...299:
     return parseJSON(response)
   case 300...399:
-    return .Failure(Result<JSON, NSError>.error("Multiple choices: \(response.code)"))
+    return .Failure(.error("Multiple choices: \(response.code)"))
   case 400...499:
-    return .Failure(Result<JSON, NSError>.error("Bad request: \(response.code)"))
+    return .Failure(.error("Bad request: \(response.code)"))
   case 500...599:
-    return .Failure(Result<JSON, NSError>.error("Server error: \(response.code)"))
+    return .Failure(.error("Server error: \(response.code)"))
   default:
-    return .Failure(Result<JSON, NSError>.error("Unknown error: \(response.code)"))
+    return .Failure(.error("Unknown error: \(response.code)"))
   }
 }
 
