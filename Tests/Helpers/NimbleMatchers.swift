34abf63fae7c0eae514aff5ad677da1ffeba9a3e
Gordon Fontenot <gmoney@gitpushgitpaid.com>
2015-12-18 21:48:55 -0800

    Fix warning in `beVoid` matcher
    Previously we weren't being explicit about the fact that this expression
    evaluation was expected to return `Void?`. Since this is a fairly
    unusual return type, Xcode was throwing warnings about it, which dirtied
    the project's build status and was generally noisy.
    
    To fix this, we can be explicit about expecting `Void?`.
    
    While I'm at it, I'll take the time to clean up the implementation a
    little.

--- Tests/Helpers/NimbleMatchers.swift
+++ Tests/Helpers/NimbleMatchers.swift
@@ -5,12 +5,10 @@ public func beVoid() -> MatcherFunc<Void> {
   return MatcherFunc { actualExpression, failureMessage in
     failureMessage.postfixMessage = "equal ()"
 
-    let actualValue = try actualExpression.evaluate()
+    let actualValue: Void? = try actualExpression.evaluate()
     switch actualValue {
-    case .Some(()):
-      return true
-    default:
-      return false
+    case ()?: return true
+    default: return false
     }
   }
 }
