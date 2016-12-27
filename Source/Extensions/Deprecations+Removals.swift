f8c0390381560155460f94db09a99edc1575e49c
Adam Sharp <adam@sharplet.me>
2016-12-27 17:03:18 -0500

    Add fix-its for renamed methods

--- Source/Extensions/Deprecations+Removals.swift
+++ Source/Extensions/Deprecations+Removals.swift
@@ -2,6 +2,20 @@ import Argo
 import Foundation
 import Result
 
+extension Client {
+  @available(*, unavailable, renamed: "perform(_:completionHandler:)")
+  func performRequest<T: Request>(_: T, completionHandler _: @escaping (Result<T.ResponseObject, SwishError>) -> ()) -> URLSessionDataTask {
+    fatalError("unavailable")
+  }
+}
+
+extension RequestPerformer {
+  @available(*, unavailable, renamed: "perform(_:completionHandler:)")
+  public func performRequest(_: URLRequest, completionHandler _: @escaping (Result<HTTPResponse, SwishError>) -> Void) -> URLSessionDataTask {
+    fatalError("unavailable")
+  }
+}
+
 extension Result {
   @available(*, unavailable, renamed: "Result.init(_:)")
   public static func fromDecoded(_: Decoded<T>) -> Result<T, SwishError> {
