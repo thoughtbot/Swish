48f7a536ba8e4e4a8e72da74780e716a787e0973
Adam Sharp <adam@sharplet.me>
2016-12-28 10:46:25 -0500

    Use a client's Scheduler for combined request completion
    Exposes `scheduler` as a public property of the `Client` protocol, to
    allow extensions to make use of it.

--- Source/Extensions/Client+CombinedRequest.swift
+++ Source/Extensions/Client+CombinedRequest.swift
@@ -4,6 +4,10 @@ extension Client {
   @discardableResult
   public func perform<A: Request, B: Request>(_ requestA: A, _ requestB: B, completionHandler: @escaping (Result<(A.ResponseObject, B.ResponseObject), SwishError>) -> Void) -> Cancelable {
     return CombinedRequest(requestA, requestB, client: self)
-      .perform(completionHandler: completionHandler)
+      .perform { [schedule = scheduler] result in
+        schedule {
+          completionHandler(result)
+        }
+      }
   }
 }
