9cf2f544b9502f7eb29d3d88b89a7bb6ffc396aa
Samuel Ryan Goodwin <samuel@roundwallsoftware.com>
2018-05-05 10:11:20 +0200

    Fix for JSON decoding with ISO6081 date format.
    The normal date formatter doesn't handle perfectly valid ISO6081 dates
    in all cases, so I need to put in this custom decoder to handle it
    properly.

--- Source/Protocols/Request.swift
+++ Source/Protocols/Request.swift
@@ -12,6 +12,8 @@ public protocol Request {
 
 public extension Request where ResponseObject: Decodable {
   func parse(_ data: Data) throws -> ResponseObject {
+    let decoder = JSONDecoder()
+    decoder.dateDecodingStrategy = .formatted(DateFormatter.iso8601Full)
     return try JSONDecoder().decode(ResponseObject.self, from: data)
   }
 }
@@ -19,4 +21,15 @@ public extension Request where ResponseObject: Decodable {
 public extension Request where ResponseObject == EmptyResponse {
   func parse(_ data: Data) throws -> ResponseObject {
   }
+}
+
+extension DateFormatter {
+  static let iso8601Full: DateFormatter = {
+    let formatter = DateFormatter()
+    formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSZZZZZ"
+    formatter.calendar = Calendar(identifier: .iso8601)
+    formatter.timeZone = TimeZone(secondsFromGMT: 0)
+    formatter.locale = Locale(identifier: "en_US_POSIX")
+    return formatter
+  }()
 }
