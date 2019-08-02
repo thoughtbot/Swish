f10ab8fb4be85429901ced9edec7dddced919e65
Ben Weitzman <benweitzman@gmail.com>
2019-08-02 19:41:25 -0400

    merge tests

--- Sources/Swoosh/Matchers/EqRequestMatcher.swift
+++ Sources/Swoosh/Matchers/EqRequestMatcher.swift
@@ -1,8 +1,12 @@
 import Swish
 
 internal struct EqRequestMatcher<T: Request>: RequestMatcher where T: Equatable {
-  func match<S>(_: S) -> Result<S.ResponseObject, SwishError>? where S: Request {
-    return response as? Result<S.ResponseObject, SwishError>
+  func match<S>(_ request: S) -> Result<S.ResponseObject, SwishError>? where S: Request {
+    if self.request == request as? T {
+      return response as? Result<S.ResponseObject, SwishError>
+    } else {
+      return .none
+    }
   }
 
   let request: T
